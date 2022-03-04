79be74def5886c2f0d4bba9c6592ba087a32cb4e
santosarker504 <79689388+santosarker504@users.noreply.github.com>
2022-03-05 02:39:52 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var a = prompt("Enter The First Number: ");
+
+var b = prompt("Enter The Second Number: ");
+
+a = parseInt(a, 10);
+b = parseInt(b, 10);
+
+var result;
+
+result = a + b;
+
+document.write(a+" + "+b+" = "+result+"<br>");
+
+
+
+result = a - b;
+
+document.write(a+" - "+b+" = "+result+"<br>");
+
+
+
+result = a * b;
+
+document.write(a+" * "+b+" = "+result+"<br>");
+
+
+
+result = a / b;
+
+document.write(a+" / "+b+" = "+result+"<br>");
+
+
+
+result = a % b;
+
+document.write(a+" % "+b+" = "+result+"<br>");
